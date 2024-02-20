247f3c167cb16d3cd6383b93058766205a44ef90
shimguh <shimguh@naver.com>
2024-02-20 12:04:01 +0900

    manual

--- manual-compose.sh
+++ manual-compose.sh
@@ -19,9 +19,9 @@ sudo docker network rm abc
 sudo docker network create abc
 
 figlet run
-sudo docker run -itd --name s-blog-a --network=abc -p 8001:80 blog-a
-sudo docker run -itd --name s-blog --network=abc -p 8002:80 blog-b
-sudo docker run -itd --name lb --network=abc -p 8000:80 lb
+sudo docker run -d --name s-blog-a --network=abc -p 8001:80 blog-a
+sudo docker run -d --name s-blog --network=abc -p 8002:80 blog-b
+sudo docker run -d --name lb --network=abc -p 8000:80 lb
 
 figlet ps
 sudo docker ps
